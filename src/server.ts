40013692b24630f94dc25abf066db7d60943a60f
Lane <>
2023-05-11 17:18:18 -0700

    js to ts

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ const app = new Hono();
 app.get("/", (ctx) => ctx.text("Hello world, this is SonicJs 2023!!"));
 
 app.get("/public/*", async (ctx) => {
-  console.log('fetch assets -->', ctx.req)
     return await ctx.env.ASSETS.fetch(ctx.req.raw);
   });
   
