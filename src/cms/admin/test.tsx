f3adaf71c4a530758a070fe5083c50f975595e02
Lane Campbell <ldc0618@gmail.com>
2023-05-12 08:50:50 -0700

    tsx test

--- src/cms/admin/test.tsx
+++ src/cms/admin/test.tsx
@@ -1,41 +1,41 @@
-import { Hono } from 'hono'
-import { html } from 'hono/html'
-import { jsx } from 'hono/jsx'
+import { Hono } from "hono";
+import { html } from "hono/html";
+import { jsx } from "hono/jsx";
 
 interface SiteData {
-    title: string
-    children?: any
-  }
+  title: string;
+  children?: any;
+}
 
-const Layout = (props: SiteData) => html`<!DOCTYPE html>
-  <html>
-    <head>
-      <title>${props.title}</title>
-    </head>
-    <body>
-      ${props.children}
-    </body>
-  </html>`
+const Layout = (props: { children?: string }) => {
+  return (
+    <html>
+      <body>{props.children}</body>
+    </html>
+  );
+};
 
-const Content = (props: { siteData: SiteData; name: string }) => (
-  <Layout {...props.siteData}>
-    <h1>Hello {props.name}</h1>
-  </Layout>
-)
-
+const Top = (props: { messages: string[] }) => {
+  return (
+    <Layout>
+      <h1>Hello Hono!</h1>
+      <ul>
+        {props.messages.map((message) => {
+          return <li>{message}!!</li>;
+        })}
+      </ul>
+    </Layout>
+  );
+};
 
 
 export async function loadTest(app) {
-console.log('---- loading test')
-    app.get('test/:name', (c) => {
-        const { name } = c.req.param()
-        const props = {
-          name: name,
-          siteData: {
-            title: 'JSX with html sample',
-          },
-        }
-        return c.html(<Content {...props} />)
-      })
+  console.log("---- loading test");
 
+  app.get("/jsx", (c) => {
+
+    const messages = ["Good Morning", "Good Evening", "Good Night"];
+    return c.html(<Top messages={messages} />);
+
+  });
 }
