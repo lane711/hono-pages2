dfeef6275112fc46cb52f2293c50063731ab3862
Lane <>
2023-05-16 16:29:04 -0700

    dash css

--- src/cms/admin/theme.tsx
+++ src/cms/admin/theme.tsx
@@ -179,6 +179,10 @@ const Layout = (props: {
 
     <script src="/public/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.1/dist/chart.umd.min.js" integrity="sha384-gdQErvCNWvHQZj6XZM0dNsAoY4v+j5P1XDpNkcM3HJG1Yx04ecqIHk7+4VBOCHOG" crossorigin="anonymous"></script>
+    <script src="/public/js/dashboard.js"></script>
+
       <script src="https://cdn.form.io/formiojs/formio.full.min.js"></script>
         <script
           src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"
